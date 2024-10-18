145bd751a04bd03c5bace346b26248499a831566
marl0vv <23glad02@mail.ru>
2024-10-18 12:01:49 +1000

    Implement helloWorld ability

--- src/main/java/com/glamik/converter_bot/ConverterBot.java
+++ src/main/java/com/glamik/converter_bot/ConverterBot.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Component;
 import org.telegram.abilitybots.api.bot.AbilityBot;
 import org.telegram.abilitybots.api.objects.Ability;
 
+import static org.telegram.abilitybots.api.objects.Locality.ALL;
+import static org.telegram.abilitybots.api.objects.Privacy.PUBLIC;
+
 @Component
 public class ConverterBot extends AbilityBot {
 
@@ -18,5 +21,14 @@ public class ConverterBot extends AbilityBot {
         return 398677200;
     }
 
-
+    public Ability sayHelloWorld() {
+        return Ability
+                .builder()
+                .name("hello")
+                .info("Says \"Hello, World!\"")
+                .locality(ALL)
+                .privacy(PUBLIC)
+                .action(ctx -> silent.send("Hello, world!", ctx.chatId()))
+                .build();
+    }
 }
