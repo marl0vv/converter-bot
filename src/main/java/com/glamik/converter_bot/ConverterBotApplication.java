631f0ec4fe84b9ae4d497b2bd12522e4a3db9f0d
marl0vv <23glad02@mail.ru>
2024-10-18 10:40:25 +1000

    Add Spring Initializr

---
+++ src/main/java/com/glamik/converter_bot/ConverterBotApplication.java
@@ -0,0 +1,13 @@
+package com.glamik.converter_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConverterBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConverterBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/glamik/converter_bot/ConverterBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glamik.converter_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConverterBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
