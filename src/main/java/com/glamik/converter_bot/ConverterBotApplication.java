de6cdf420996dc0020ad15777fa99c7b2358ce2e
marl0vv <23glad02@mail.ru>
2024-10-18 16:13:46 +1000

    Add eureka client

--- src/main/java/com/glamik/converter_bot/ConverterBotApplication.java
+++ src/main/java/com/glamik/converter_bot/ConverterBotApplication.java
@@ -3,11 +3,13 @@ package com.glamik.converter_bot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 import org.telegram.abilitybots.api.bot.AbilityBot;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.starter.TelegramBotStarterConfiguration;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @SpringBootApplication
