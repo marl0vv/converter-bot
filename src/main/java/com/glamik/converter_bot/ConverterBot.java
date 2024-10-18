84d35d2a309bf2abbbb88912e7ec05ce520b64cc
marl0vv <23glad02@mail.ru>
2024-10-18 14:38:34 +1000

    Add function that replies to image

--- src/main/java/com/glamik/converter_bot/ConverterBot.java
+++ src/main/java/com/glamik/converter_bot/ConverterBot.java
@@ -3,10 +3,19 @@ package com.glamik.converter_bot;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.abilitybots.api.bot.AbilityBot;
+import org.telegram.abilitybots.api.bot.BaseAbilityBot;
 import org.telegram.abilitybots.api.objects.Ability;
+import org.telegram.abilitybots.api.objects.Flag;
+import org.telegram.abilitybots.api.objects.Reply;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 
 import static org.telegram.abilitybots.api.objects.Locality.ALL;
 import static org.telegram.abilitybots.api.objects.Privacy.PUBLIC;
+import static org.telegram.abilitybots.api.util.AbilityUtils.getChatId;
 
 @Component
 public class ConverterBot extends AbilityBot {
@@ -30,5 +39,22 @@ public class ConverterBot extends AbilityBot {
                 .privacy(PUBLIC)
                 .action(ctx -> silent.send("Hello, world!", ctx.chatId()))
                 .build();
+    }
+
+    public Ability greetUser() {
+        return Ability
+                .builder()
+                .name("start")
+                .info("Greets user at the start of the bot's work")
+                .locality(ALL)
+                .privacy(PUBLIC)
+                .action(ctx -> silent.send("Welcome to the WebpConverterBot! Send the image you want to convert.", ctx.chatId()))
+                .build();
+    }
+
+    public Reply sayYuckOnImage() {
+        BiConsumer<BaseAbilityBot, Update> action = (bot, upd) -> silent.send("Yuck", getChatId(upd));
+
+        return Reply.of(action, Flag.PHOTO);
     }
 }
